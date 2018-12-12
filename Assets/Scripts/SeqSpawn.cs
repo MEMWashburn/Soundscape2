fc5c2407625c1570a227d44a61990b40f9c2b766
Megan Washburn <memwashburn@gmail.com>
2018-12-11 21:51:11 -0800

    Starting sequencer, need to add visualizer and specific track support, as well as daytime / nightime shift from playground to sequencer

---
+++ Assets/Scripts/SeqSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeqSpawn : MonoBehaviour {
+    public GameObject[] smackables;
+    public Transform[] smackPoints;
+    public float beat = 1.14285714286f; // (60 / 105) * 2;
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > beat)
+        {
+            GameObject smack = Instantiate(smackables[Random.Range(0, 2)], smackPoints[Random.Range(0, 4)]);
+            smack.transform.localPosition = Vector3.zero;
+            smack.transform.Rotate(transform.forward, 67.5f); // 90 - (45 / 2));
+            smack.transform.Rotate(transform.up, 90);
+            timer -= beat;
+        }
+
+        timer += Time.deltaTime;
+	}
+}
---
+++ Assets/Smacker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smacker : MonoBehaviour {
+
+    public LayerMask layer;
+    public Vector3 prevPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit smack;
+
+        if (Physics.Raycast(transform.position, transform.forward, out smack, 1, layer))
+        {
+            if (Vector3.Angle(transform.position - prevPos, smack.transform.up) > 130) {
+                Destroy(smack.transform.gameObject);
+            }
+        }
+        prevPos = transform.position;
+	}
+}
